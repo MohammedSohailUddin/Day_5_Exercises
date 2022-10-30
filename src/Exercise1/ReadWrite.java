d552ce0ab2e91bcc5c528cb4412beddb23f0aad2
Mohammed Sohail Uddin <70440734+MohammedSohailUddin@users.noreply.github.com>
2022-10-30 17:51:21 +0530

    only one try and catch

--- src/Exercise1/ReadWrite.java
+++ src/Exercise1/ReadWrite.java
@@ -3,26 +3,24 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 class ReadWrite {
   public static void main(String[] args) {
-    //writing to the file
+	  //writing to the file
     try {
       PrintWriter output = new PrintWriter("C:\\Users\\Mohammed SohailUddin\\Desktop\\try\\output.txt");
       String name = "Mohammed Sohail Uddin";
       output.printf("My full name is: " +name);
       output.close();
-    }
-    catch(IOException e) {
-      System.out.println("Exception caught is: "+e);
-    }
+      
     //reading the same file
-    BufferedReader objReader = null;
-    try {
+     BufferedReader objReader = null;
      String input;
      objReader = new BufferedReader(new FileReader("C:\\Users\\Mohammed SohailUddin\\Desktop\\try\\output.txt"));
      while ((input = objReader.readLine()) != null) {
       System.out.println(input);
      }
+     objReader.close();
     }
     catch (IOException e) {
         System.out.println("Exception caught is: "+e);
